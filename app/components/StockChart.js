b0d071b3f5f6bce8d94b2f1c733d0181e4b3de30
Ishana Jain <ishanajain99@gmail.com>
2025-03-10 13:29:53 -0700

    Fixed commenting

--- app/components/StockChart.js
+++ app/components/StockChart.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
 import { Dimensions, Text } from 'react-native';
 import { LineChart } from 'react-native-chart-kit';
-import { ACTUAL_API_KEY as API_KEY} from '@env'; // Make sure you've set up your API key in an .env file
+import { ACTUAL_API_KEY as API_KEY} from '@env'; 
 
