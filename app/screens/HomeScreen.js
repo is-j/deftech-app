bb9ed98402d77dcee41d9c4a1cf6bb1cf430da2f
Ishana Jain <ishanajain99@gmail.com>
2025-03-12 20:45:17 -0700

    Updated Homescreen - added functionality

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
 import {getDailyStockData, getCompanyOverview, getQuote} from '../services/alphaVantageService'
 
 
@@ -41,7 +41,106 @@ const DEFENSE_STOCKS=[
     description: 'Global aerospace and defense technology company specializing in autonomous systems and space',
   },
 ];
-const HomeScreen = () => {
+const HomeScreen = ({navigation}) => {
+
+  const[searchQuery, setSearchQuery] = useState('');
+  const[selectedCategory, setSelectedCategory] = useState('all');
+  const[stocksData, setStocksData] = useState([]);
+  const[loading, setLoading] = useState(true);
+  const[error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchStockData = async () => {
+      try{
+        setLoading(true);
+        setError(null);
+
+        const publicStocks = DEFENSE_STOCKS.filter(stock => stock.symbol !=='PRIVATE');
+
+        const promises = publicStocks.map(stock => getQuote(stock.symbol));
+
+        const results = await Promise.all(promises);
+
+        const updatedStocks = DEFENSE_STOCKS.map(stock => {
+
+          if(stock.symbol == 'PRIVATE'){
+            return {...stock, price: 'N/A', change: 'N/A', changePercent: 'N/A'};
+          }
+
+          const index = publicStocks.findIndex(s => s.symbol === stock.symbol);
+          const quoteData = results[index]['Global Quote'];
+
+          if(quoteData){
+            const price = parseFloat(quoteData['0.5. price']).toFixed(2);
+            const change = parseFloat(quoteData['0.9. change']).toFixed(2);
+            const changePercent = quoteData['10. change percent'].replace('%', '');
+
+            return{
+              ...stock,
+              price,
+              change: change >= 0 ? '+${change}' : change,
+              changePercent: '${changePercent}%'
+            };
+
+          }
+
+          return{...stock, price: 'N/A', change: 'N/A', changePercent: 'N/A'};
+        });
+
+        setStocksData(updatedStocks);
+        setLoading(false);
+
+      } catch (error){
+        console.error('error fetching stock data: ', error);
+        setError('Failed to fetch stock data, try again later');
+        setLoading(false);
+
+        setStocksData(DEFENSE_STOCKS.map(stock => ({
+          ...stock,
+          price: 'N/A',
+          change: 'N/A',
+          changePercent: 'N/A'
+        })));
+      }
+    };
+
+    fetchStockData();
+
+    const intervalId = setInterval(fetchStockData, 5*60*1000);
+
+    return () => clearInterval(intervalId);
+
+  },[]);
+
+  const filteredStocks = stocksData.filter(stock  => {
+
+    const matchesSearch = stock.name.toLowerCase().includes(searchQuery.toLowerCase()) || stock.symbol.toLowerCase().includes(searchQuery.toLowerCase());
+
+    const matchesCategory = selectedCategory === 'all' || stock.category === selectedCategory;
+
+    return matchesSearch && matchesCategory;
+  });
+
+  const renderStockItem = ({item}) => (
+    <TouchableOpacity style={styles.stockCard} onPress ={() => navigation.navigate('Details', {stock: item})} >
+      <View>
+        <Text>{item.name}</Text>
+        <Text>{item.symbol}</Text>
+      </View>
+      <Text>{item.description}</Text>
+      <View>
+        {item.symbol}!== 'PRIVATE' ? (
+          {item.price} !== 'N/A' ? (
+            <>
+              <Text>${item.price}</Text>
+              <Text></Text>
+            
+            </>
+          )
+        )
+      </View>
+    </TouchableOpacity>
+  )
   return(
     <View style={styles.container}>
       <Text style={styles.text}>Defense Stock Home Screen</Text>
