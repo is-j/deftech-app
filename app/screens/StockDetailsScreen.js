674efa5f99d9e9652eecb4d92585cde5904b2792
Ishana Jain <ishanajain99@gmail.com>
2025-03-14 20:50:15 -0700

    Added filter based on time

--- app/screens/StockDetailsScreen.js
+++ app/screens/StockDetailsScreen.js
@@ -55,6 +55,62 @@ const StockDetailsScreen = ({route, navigation}) => {
 
     }, [stock, navigation]);
 
+    const getChartData = () => {
+        if(!historicalData || !historicalData.results){
+            return null;
+        }
+        const results = [...historicalData.results].reverse();
+
+        let filteredResults;
+        const now = new Date();
+
+        switch(timeRange){
+            case '1D':
+                filteredResults = results.slice(-1);
+                break;
+            case '1W':
+                filteredResults = results.slice(-5);
+                break;
+            case '1M':
+                filteredResults = results.slice(-22);
+                break;
+            case '3M':
+                filteredResults = results.slice(-66)
+                break;
+            case '1Y':
+                filteredResults = results;
+                break;
+            default:
+                filteredResults = results.slice(-5);
+        }
+
+        //extracting closing price/date
+        const prices = filteredResults.map(day => day.c);
+
+        //labels
+        const dates = filteredResults.map(day => {
+            const date = new Date(day.t);
+            return `${date.getMonth() + 1}/${date.getDate()}`;
+        });
+        
+        //make sure no overcrowd
+        const labelCount = Math.min(5, dates.length);
+        const step = Math.floor(dates.length/labelCount);
+        const labels = dates.filter((_, i) => i % step === 0);
+
+        return{
+            labels, 
+            datasets: [
+                {
+                    data: prices,
+                    color: (opacity = 1) => prices[0] < prices[prices.length - 1] ? 
+                    `rgba(0,255,0 ${opacity})` : `rgba(255, 0,0 ${opacity})`,
+                    strokeWidth: 2
+                }
+            ]
+        };
+    };
+
     return(
         <View style={styles.container}>
             <Text> style={styles.text}Stock Details Screen</Text>
